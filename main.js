63bbb9cfb5cca627cec0ac15301261f0101d0167
Anton Öberg <oberg.anton@gmail.com>
2016-02-08 17:01:25 +0100

    call render method on ReactDOM

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //entry into webbapplication
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-React.ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
