0466356298f22baef5e45155d2bfa0c4fc8ab00e
robine81 <robinew@Robines-MacBook-Air.local>
2016-02-08 16:48:32 +0100

    First time upload

---
+++ App.js
@@ -0,0 +1,10 @@
+//first REACT component
+
+import	React from	'react';
+class App extends React.component {
+	render(){
+		return <div>Hello world</div>
+	}
+}
+
+export default App
---
+++ main.js
@@ -0,0 +1,6 @@
+//entry into webbapplication
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+React.ReactDOM.render(<App />, document.getElementById('app'))
